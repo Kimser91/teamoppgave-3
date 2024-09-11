18cf5f2d2ecf29aa58eb6820250698445ba45afb
Kamillaaks <kammi.ar@hotmail.com>
2024-09-11 13:48:32 +0200

    Update view.js

--- view.js
+++ view.js
@@ -4,6 +4,7 @@ function updateView()
     html.innerHTML = /*HTML*/ `
     <div>Hei</div>
     `
+    winOrLoseCheck()
 } 
 
 
@@ -161,4 +162,29 @@ function getRandomObject() {
             document.getElementById('car__back').style.backgroundColor == 'red';
         }
     
+    }
+
+    function winOrLoseCheck() {
+        if(points == win) {
+            winFunction();
+        }
+        else if(points == lose) {
+            loseFunction();
+        }
+    }
+    
+    function winFunction() {
+        html.innerHTML = /*HTML*/ `
+        <div>Gratulerer, du vant spillet!!</div>
+        <button onclick="Location.reload()">Spill igjen</button>
+        <img src="https://tenor.com/view/snoop-dogg-dancing-fun-funny-dance-dance-moves-gif-6648927863143953704" alt="Snoop Dogg danser fordi du vant <3">
+        `
+    }
+    
+    function loseFunction() {
+        html.innerHTML = /*HTML*/ `
+        <div>Lol du tapte, noob</div>
+        <button onclick="Location.reload()">Spill igjen</button>
+        <img src="https://tenor.com/view/kto-kounotori-kounotoritoken-lbow-storkholders-gif-25676479" alt="Du er en taper >:(">
+        `
     }
