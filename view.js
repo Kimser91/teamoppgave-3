ebab227540757d4c7066a179df50507c00508e4b
MiiNK1Y <98641699+MiiNK1Y@users.noreply.github.com>
2024-09-12 13:34:34 +0200

    driving sign

--- view.js
+++ view.js
@@ -36,6 +36,9 @@ function updateView()
             <br>
             <button onclick="startDriving()">wanna drive?</button>
         </div>
+        <div>
+            <div class="sign">100 km</div>
+        </div>
     
     </div>
     <div id="you-found">
@@ -47,6 +50,14 @@ function updateView()
     console.log(points);
     winOrLoseCheck();
 } 
+
+
+function startDriving() 
+{
+    let r = Math.floor(Math.random()*100);
+    if(r < 10){getRandomPerson()}
+    else if(r >= 10){getRandomObject()}
+}
 
 function getRandomPerson() {
     let random = Math.floor(Math.random() * 4)
@@ -74,6 +85,7 @@ function getRandomPerson() {
     } else {getRandomPerson()};
 
 }
+
 
 function getRandomObject() {
     let random = Math.floor(Math.random() * 5);
@@ -131,6 +143,26 @@ function wrongGreeting(){
     startDriving()
 }
 
+function acceptItem(){
+    if (document.getElementById("you-found1").innerHTML.includes("Sprayboks")) 
+        {
+            number++
+            points = points + 5
+            changeColor();
+            startDriving()
+            console.log(number)
+        } else {
+            points++
+            updateView()
+            startDriving()
+         }
+}
+
+function declineItem(){
+    updateView()
+    startDriving()
+}
+
 function changeColor() {
     let hardColor = 'red';
     if (frontColor != hardColor) {
@@ -149,6 +181,14 @@ function changeColor() {
         backColor = hardColor;
     }
     updateView();
+}
+
+function winOrLoseCheck() {
+    if(points >= win) {
+        winFunction();
+    } else if (points <= lose) {
+        loseFunction();
+    }
 }
 
 function winFunction() {
